5b0ae9fe9f09a58ab0a0aebee6c0646f257f012c
Martin Vincent <martin.vincent.dk@gmail.com>
2015-09-17 10:16:25 +0200

    minor correction

--- tests/msgl_grouping_test_1.R
+++ tests/msgl_grouping_test_1.R
@@ -11,14 +11,14 @@ classes <- sim.data$classes
 grouping <- rep(1:4, 100)
 
 ## Lambda sequence
-lambda <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = 0, d = 100L, lambda.min = 0.05, standardize = FALSE)
-lambda1 <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = 0, d = 100L, lambda.min = 0.05, sparse.data = TRUE, standardize = FALSE)
+lambda <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = 0, d = 100L, lambda.min = 0.01, standardize = FALSE)
+lambda1 <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = 0, d = 100L, lambda.min = 0.01, sparse.data = TRUE, standardize = FALSE)
 if(max(abs(lambda-lambda1)) > 1e-5) stop()
 
-lambda <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = .5, d = 100L, lambda.min = 0.05, standardize = FALSE)
-lambda1 <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = .5, d = 100L, lambda.min = 0.05, sparse.data = TRUE, standardize = FALSE)
+lambda <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = .5, d = 100L, lambda.min = 0.01, standardize = FALSE)
+lambda1 <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = .5, d = 100L, lambda.min = 0.01, sparse.data = TRUE, standardize = FALSE)
 if(max(abs(lambda-lambda1)) > 1e-5) stop()
 
-lambda <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = 1, d = 100L, lambda.min = 0.05, standardize = FALSE)
-lambda1 <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = 1, d = 100L, lambda.min = 0.05, sparse.data = TRUE, standardize = FALSE)
+lambda <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = 1, d = 100L, lambda.min = 0.01, standardize = FALSE)
+lambda1 <- msgl.lambda.seq(x, classes, grouping = grouping, alpha = 1, d = 100L, lambda.min = 0.01, sparse.data = TRUE, standardize = FALSE)
 if(max(abs(lambda-lambda1)) > 1e-5) stop()
