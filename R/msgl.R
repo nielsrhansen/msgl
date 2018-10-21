bb493fc0df7f3f1b74fa23c45d67884bb67e928b
nielsrhansen <niels.r.hansen@math.ku.dk>
2018-10-22 00:04:03 +0200

    Examples and documentation

--- R/msgl.R
+++ R/msgl.R
@@ -24,14 +24,19 @@
 #' # A quick look at the data
 #' dim(x)
 #' table(classes)
+#' 
+#' # A smaller subset with three classes 
+#' small <- which(classes %in% c("CCA", "CRC", "Pancreas"))
+#' classes <- classes[small, drop = TRUE]
+#' x <- x[small, ]
 #'
 #' #Do cross validation using 2 parallel units
 #' cl <- makeCluster(2)
 #' registerDoParallel(cl)
 #'
-#' # Do 10-fold cross validation on a lambda sequence of length 100.
-#' # The sequence is decreasing from the data derived lambda.max to 0.5*lambda.max
-#' fit.cv <- msgl::cv(x, classes, alpha = .5, lambda = 0.5, use_parallel = TRUE)
+#' # Do 4-fold cross validation on a lambda sequence of length 100.
+#' # The sequence is decreasing from the data derived lambda.max to 0.2*lambda.max
+#' fit.cv <- msgl::cv(x, classes, fold = 4, lambda = 0.2, use_parallel = TRUE)
 #'
 #' stopCluster(cl)
 #'
